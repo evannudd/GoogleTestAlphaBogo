30854261ecea02a66a0a8b1d6ac9ec3ed395eb44
enuddy <evan.nudd@mymail.champlain.edu>
2018-04-13 08:54:04 -0400

    added functions to run program

--- AlphaBogo/Game.cpp
+++ AlphaBogo/Game.cpp
@@ -1,6 +1,8 @@
 #include "Game.h"
 #include <random>
 #include <algorithm>
+#include <iostream>
+#include <ctime>
 
 Game::Game()
 {
@@ -14,6 +16,38 @@ Game::~Game()
 void Game::start()
 {
 	isRunning = true;
+
+	// populate the game list with random ints.
+	for (int i = 0; i < 10; i++) {
+		myList.push_back(rand() % 100);
+	}
+}
+
+void Game::run()
+{
+	// populate the game list and start isRunning.
+	start();
+
+	// display the list.
+	for (int i = 0; i < myList.size(); i++) {
+		std::cout << myList[i];
+	}
+
+	// Time how long it takes for the user to solve it.
+	std::time_t time_now = time(0);
+	userSort();
+	std:time_t time_after = time(0);
+	std::time_t totalUserTime = time_after - time_now;
+	
+	// time how long it takes for AlphaBogo to solve it.
+	std::time_t time_now = time(0);
+	bogoSort(myList);
+	std:time_t time_after = time(0);
+	std::time_t totalUserTime = time_after - time_now;
+}
+
+void Game::userSort() {
+
 }
 
 std::vector<int> Game::bogoSort(std::vector<int> list)
--- AlphaBogo/Game.h
+++ AlphaBogo/Game.h
@@ -5,12 +5,15 @@ class Game
 {
 private:
 	bool isRunning = false;
+	std::vector<int> myList;
 
 public:
 	Game();
 	~Game();
 
 	void start();
+	void run();
+	void userSort();
 
 	std::vector<int> bogoSort(std::vector<int> list);
 	std::vector<int> sort(std::vector<int> list);
